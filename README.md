95d675218fb6e5db360216d93a955c2a6a661bae
Victor Torres <victorhxtorres@gmail.com>
2012-02-13 14:14:06 -0200

    Added validType() and type support

--- README.md
+++ README.md
@@ -61,9 +61,48 @@ You've 3 ways to implement jsget in your page.
 ```
 
 
+### Working with js types ###
+
+Jsget `get(param)` will always return false or a string. In some situations you could need a primitive or complex js type that was the really intention for a GET value (i.e in ?foo=12.3 `get('foo')` should return 12.3 as `number` type)
+
+To get a value as it "should be" type, just use the second parameter with `get()`. Jsget'll try to change the original type (string) to the defined one.
+
+**file.js?foo=12.3**
+
+```html
+<script>
+	var r = new Jsget;
+	document.write(r.get('foo', 'float')); // returns 12.3 as a numeric type
+</script>
+```
+
+Also, if you wanna a strict type validation (i.e I need foo to receive an integer type, any other type should be an error), `validType` could help. This function will validate the type you defined with the true type that jsget will retrieve.
+
+**file.js?foo=12**
+
+```html
+<script>
+	var r = new Jsget;
+
+	// Jsget will first transform (string)12 to (number)12 and next validate with 'integer' type. Any type incompatibility will return false. If types are ok, just return 12 (as number object).
+	document.write(r.validType('foo', 'integer'));
+</script>
+```
+
+The second param from `get()` and `validType()` share the same configuration. Jsget support these:
+
+* __null__ To empty strings like `?foo=`
+* __float__ Floating point numbers like `?foo=12.3`
+* __integer__ Integer numbers `?foo=12`
+* __object__ You can also use string objects (like JSON) `?foo={a:'b',c:'d'}`
+* __array__ Arrays looks like objects, but more simple `?foo=['one','two','three']`
+
+Undefined type is useless here because all functions need to verify the existence of all keys called.
+
 ### Limitations ###
 
 Javascript files can't know their own filename, only the page which loaded them. This is a language limitation, so like explained at `How it works` section, jsget needs to walk with DOM construction and read the last &lt;script&gt; tag loaded (which is the DOM pointer position).
+
 What is this about? You can't use jsget inside ready or load statements. Look:
 
 **file.js?foo=bar**
@@ -109,5 +148,4 @@ What is this about? You can't use jsget inside ready or load statements. Look:
 ```
 
 In `window.load` example above it should not return what I expect. What 'write' method will return then?
-Simple. When using ready/load statements, you tell the script to wait until full DOM (or even more, like images) is loaded. If DOM is loaded and jsget is called inside a ready/load statement, it will get the latest &lt;script&gt; written in your HTML document. Maybe this ready/load script could be your last &lt;script&gt; tag inside document. **Avoid it**
-
+Simple. When using ready/load statements, you tell the script to wait until full DOM (or even more, like images) is loaded. If DOM is loaded and jsget is called inside a ready/load statement, it will get the latest &lt;script&gt; written in your HTML document. Maybe this ready/load script could be your last &lt;script&gt; tag inside document. **Avoid it**.
--- jsget.js
+++ jsget.js
@@ -4,7 +4,7 @@
  * Allows querystring use inside .js files and <script> tags
  * 
  * @author	Victor Torres - talk@victortorr.es
- * @version	0.8
+ * @version 1.0b
  * @since	02/2012
  * @param 	string	file Optional. If empty, js script itself. If a string url is passed (with querystring), jsget will read it instead of page or .js url.
  * @returns	Object
@@ -18,74 +18,201 @@
 function Jsget(file) {
 	
 	// Object public attributes and "methods" (js doesn't have methods, but functions as methods [or something like])
-	/**
-	 * queryString
-	 * 
-	 * Contains the original querystring from file. Just for basic user access
-	 */
-	this.queryString = '';
 	
 	/**
 	 * get
 	 * 
-	 * Return all or one parameter from url querystring.
+	 * Returns all or one parameter from url querystring.
 	 * If key's empty, return the entire object (keys and values) of querystring (__params)
 	 * if key isn't found, return false
 	 * then, return the key value
 	 * 
 	 * @param	string	key
+	 * @param	string	type	Tries to define an acceptable type for key value
 	 * @return	mixed
 	 */
-	this.get = function(key) {
+	this.get = function(key, type) {
 		if(key === undefined)
 			return __params;
 		else if(__params[key] === undefined)
 			return false;
-		else
-			return __params[key];
+		else {
+			// good, we have a valid key. Now, if the 2nd param is setted, we need to try a valid type
+			if(type !== undefined) {
+				var find = __findType(__params[key], type); // Warning: If the defined type is not the real key type, returns the string key value anyway as string (for forceful, check validType()). If error, false (check console)
+				return find.value;
+			}
+			else // No type was especified. Just return the key value (as string)
+				return __params[key];
+		}
+	};
+	
+	/*
+	 * validType
+	 * 
+	 * Returns the value of a key if it has the same type of parameter string
+	 * Undefined does not exists because get() will already check if key exists.
+	 * NULL means empty string (i.e '?foo=')
+	 * 
+	 * @param	string	key
+	 * @param	string	type	A primitive/complex javascript type ['null','string','integer','float','array','object']
+	 * @return	string|bool	key value or false if not valid
+	 * @example	validType('foo', 'string')
+	 * @todo	string ok but how about numeric? Could it be splitted in intger and float?
+	 * @todo	any bool form?
+	 */
+	this.validType = function(key, type) {
+		var valid = ['null','string','integer','float','array','object'];
+		type = type.toLowerCase();
+		
+		// Check if type parameter has a valid value
+		if(valid.indexOf(type) === -1) { // no match inside array
+			// This is used 2 times. Better put in a private method
+			if(typeof(console) === 'object')
+				console.error('The "type" parameter is invalid, please check documentation. Valid values: '+valid.join(', '));
+			return false;
+		}
+		
+		// Param validation ok, attempt to retrieve value type
+		var v = __findType(key, type);
+		//console.log(typeof(v.value)+' = '+type);
+		
+		// Just check __findType() and type
+		if(typeof(v.value) !== type)
+			return false;
+		
+		// Ok, true type and param type are the same
+		return v.value;
+	};
+	
+	/**
+	 * toString
+	 * 
+	 * Returns the original (but encoded) querystring from file url.
+	 * 
+	 * @return 	string
+	 */
+	this.toString = function() {
+		return __queryString;
 	};
 	
 	/************* PRIVATE STATEMENTS *************/
-	var __params = {};
+	var __queryString = ''; // Contains the original querystring from file. Just for basic user access
+	var __params = {}; // Contains the valid key=>value objects of splitted querystring
+	
+	/**
+	 * __findType
+	 * 
+	 * Tries to find a cohesive type to passed str]
+	 * 
+	 * @param	string	str		A value (new String() object)
+	 * @return	mixed	If error, false. Anything else {type, value}
+	 * @todo	Object and array... seems to be the same
+	 */
+	var __findType = function(str) {
+		// Creates object and tries to get the type of 'str'
+		var ret = {type: null, value: null};
+		
+		// 1st: null
+		if(!str) {
+			ret.type = 'null';
+			ret.value = null;
+		}
+		// 2nd: float (it accepts ,) and number. Note: If 1,3a, javascript will try to fix it with parseFloat() to 1.3 
+		else if(!isNaN(parseFloat(str.replace(',', '.')))){ // @todo perhaps a var = parseFloat...
+			var floatTest = parseFloat(str.replace(',', '.'));
+			
+			// check if has a dot to confirm a float value
+			if(floatTest.toString().split('.').length === 2) {
+				ret.type = 'float';
+				ret.value = floatTest; // real type will be number
+			}
+			
+			else {
+				ret.type = 'integer';
+				ret.value = floatTest;
+			}
+			//console.log((!isNaN(parseInt(str)) ? parseInt(str) : 'NaN'));
+			//console.log(str+' typeof = '+typeof(floatTest)+' parseFloat = '+parseFloat(floatTest)+' parseInt = '+parseInt(floatTest));
+		}
+		// Object
+		else if(str.charAt(0) === '{' && str.charAt(str.length-1) === '}') {
+			var objectTest = eval('('+str+')');
+			
+			ret.type = 'object';
+			ret.value = objectTest;
+		}
+		// Array
+		else if(str.charAt(0) === '[' && str.charAt(str.length-1) === ']') {
+			var arrayTest = eval('('+str+')'); // Array do not have indexes
+			
+			ret.type = 'array';
+			ret.value = arrayTest;
+		}
+		
+		else { // Anything else is a string, the default type. If you think it's wrong or should be something else, could be because a malformed object/array string
+			ret.type = 'string';
+			ret.value = str;	
+		}
+		
+		// Return new typed var
+		return ret;
+	};
 	
 	/*********** __CONSTRUCT *********************/
-	// if no file is specified at constructor param and no src attr is found (empty means code between <script></script>), then get document itself
-	if(file === undefined && document.scripts[document.scripts.length-1].src != '')
-		file = document;
-	else {
-		// string or selfscript
-		var newfile = new Object(); // {}
-		newfile.src = (typeof(file) === 'string' ?
-												file // if string, simulates document scripts attribute as object
-												: document.location.href); // anything else means you're using this class inside a <script> tag in your html. So, we get the page base url. Our file now assumes the own text/html document
-		file = {scripts: [newfile]};
-	}
-	
-	// get only querystring from file object
-	this.queryString = file.scripts[file.scripts.length-1].src.replace(/^[^\?]+\??/,'');
-	
-	// if doesn't exists (i.e malformed url), class ends here
-	if(!this.queryString || this.queryString === '') {
-		// if console, generates error
-		if(typeof(console) === 'object') {
-			console.error('Unable to load your querystring. Check it.', this);
+	(function(){
+		// if no file is specified at constructor param and no src attr is found (empty means code between <script></script>), then get document itself
+		if(file === undefined && document.scripts[document.scripts.length-1].src !== '')
+			file = document;
+		else {
+			// string or selfscript
+			var newfile = new Object(); // {}
+			newfile.src = (typeof(file) === 'string' ?
+													file // if string, simulates document scripts attribute as object
+													: document.location.href); // anything else means you're using this class inside a <script> tag in your html. So, we get the page base url. Our file now assumes the own text/html document
+			file = {scripts: [newfile]};
 		}
-		return;
-	}
+		
+		// get only querystring from file object
+		__queryString = file.scripts[file.scripts.length-1].src.replace(/^[^\?]+\??/,'');
+		
+		// if doesn't exists (i.e malformed url), class ends here
+		if(!__queryString || __queryString === '') {
+			// if console, generates error
+			if(typeof(console) === 'object') {
+				console.error('Unable to load your querystring. Check it.', this);
+			}
+			return;
+		}
+			
+			
+		// splitting &
+		var p = __queryString.split(/[;&]/);
+		
+		var keyVal;
+		
+		// generates array[key] = val
+		for(var i=0;i<p.length;i++) {
+			keyVal = p[i].split('=');
+			if(!keyVal || keyVal.length !== 2)
+				continue;
+			
+			__params[unescape(keyVal[0])] = unescape(keyVal[1]).replace(/\+/g, ' ');
+		}
 		
 		
-	// splitting &
-	var p = this.queryString.split(/[;&]/);
+		// IE indexOf fix
+		if(!Array.indexOf){
+		    Array.prototype.indexOf = function(obj){
+		        for(var i=0; i<this.length; i++){
+		            if(this[i] === obj){
+		                return i;
+		            }
+		        }
+		        return -1;
+		    };
+		}
+	}());
 	
-	var keyVal;
-	
-	// generates array[key] = val
-	for(var i=0;i<p.length;i++) {
-		keyVal = p[i].split('=');
-		if(!keyVal || keyVal.length != 2)
-			continue;
-		
-		__params[unescape(keyVal[0])] = unescape(keyVal[1]).replace(/\+/g, ' ');
-	}
 	
 }
